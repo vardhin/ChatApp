a7a5b0c016241afde444e956ade9c06414f9b035
Vardhin <115967495+vardhin@users.noreply.github.com>
2024-05-02 23:38:17 +0530

    Update RSAencrypter.py

--- RSAencrypter.py
+++ RSAencrypter.py
@@ -11,34 +11,41 @@ def decrypt_message(private_key, encrypted_message):
     decrypted_message = cipher.decrypt(encrypted_message)
     return decrypted_message.decode()
 
-# Let's simulate the scenario where A gives their public key to B
-# and B gives their public key to A. In a real-world scenario,
-# you'd use something like sockets or a messaging service to exchange keys.
+def gen_keys():
+    private_key = RSA.generate(2048)
+    public_key = private_key.publickey()
+    return private_key, public_key
+
+print("Let's simulate the scenario where A gives their public key to B")
+print("and B gives their public key to A. In a real-world scenario,")
+print("you'd use something like sockets or a messaging service to exchange keys. \n\n\n")
 
 # Generating A's key pair
-key_pair_A = RSA.generate(2048)
-public_key_A = key_pair_A.publickey()
-private_key_A = key_pair_A.export_key()
+private_key_A, public_key_A = gen_keys()
+print(f"private key of A is = {private_key_A}")
+print(f"public key of A is = {public_key_A}\n\n")
 
 # Generating B's key pair
-key_pair_B = RSA.generate(2048)
-public_key_B = key_pair_B.publickey()
-private_key_B = key_pair_B.export_key()
+private_key_B, public_key_B = gen_keys()
+print(f"private key of B is = {private_key_B}")
+print(f"public key of B is = {public_key_B}\n\n")
 
 # Let's simulate B sending a message to A
 message_from_B = "Hey A, it's me B. How are you?"
+print(f"message from B is = {message_from_B}\n")
 encrypted_message_for_A = encrypt_message(public_key_A, message_from_B)
+print(f"the encrypted message for A is = {encrypted_message_for_A}\n\n")
 
-# Now, A decrypts the message from B
+
+print("Now, A decrypts the message from B")
 decrypted_message_by_A = decrypt_message(private_key_A, encrypted_message_for_A)
+print("Message from B decrypted by A:", decrypted_message_by_A,"\n\n")
 
-print("Message from B decrypted by A:", decrypted_message_by_A)
-
-# Simulating A sending a message to B
+print("Simulating A sending a message to B")
 message_from_A = "Hey B, I'm doing great! How about you?"
 encrypted_message_for_B = encrypt_message(public_key_B, message_from_A)
+print(f"message from A: {message_from_A}\n\n")
 
-# Now, B decrypts the message from A
+print("Now, B decrypts the message from A")
 decrypted_message_by_B = decrypt_message(private_key_B, encrypted_message_for_B)
-
 print("Message from A decrypted by B:", decrypted_message_by_B)
